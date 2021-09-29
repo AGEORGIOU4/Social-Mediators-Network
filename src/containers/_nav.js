9825bbf9fd4aefc8f089c36318234d1d84ae0e60
Andreas Georgiou <ageorgiou4@uclan.ac.uk>
2021-09-29 06:03:19 +0300

    full project

---
+++ src/containers/_nav.js
@@ -0,0 +1,14 @@
+const _nav = [
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Dashboard',
+    to: '/dashboard',
+    icon: 'cil-speedometer',
+    badge: {
+      color: 'info',
+      text: 'NEW',
+    }
+  },
+]
+
+export default _nav
