eac3cd3c88a7d86389eefb911d134a247a436a6e
Andreas Georgiou <AGeorgiou4@uclan.ac.uk>
2021-11-05 01:03:15 +0200

    Blog Page

---
+++ src/Tables/CommentsTable.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import { CButton, CCol, CDataTable, CCard, CCardBody, CImg } from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilPencil, cilTrash } from '@coreui/icons'
+import { Route } from "react-router-dom";
+import { cisEye } from '@coreui/icons-pro';
+import { FormatTimestamp } from 'src/reusable/reusables';
+
+
+export const commentsFields = [
+  { key: 'card', label: "", sorter: false, filter: false },
+  { key: 'author' },
+  { key: 'content' },
+]
+
+export const CommentsTable = (props) => {
+  return (
+    <CCol xs="12">
+      <Route render={({ history }) => (
+        <CDataTable
+          items={props.comments}
+          fields={commentsFields}
+          loading={props.loading}
+          header={false}
+          // tableFilter={{ 'placeholder': 'Search...' }}
+          itemsPerPage={20}
+          pagination
+          clickableRows
+          sorterValue="createdAt"
+          scopedSlots={{
+            'card':
+              (item) => (
+                <td>
+                  {/* <CCard style={{ padding: "0", margin: "0" }}>
+                    <CCardBody> */}
+                  <div style={{ backgroundColor: "#f2f2f2", borderRadius: "5px", padding: '15px' }}>
+                    <div style={{ width: '100%' }}>
+                      <div style={{ width: "20%", float: 'left', textAlign: "center", marginLeft: '-6px', marginRight: '6px' }}>
+                        <CImg src={(item.picture) ? item.picture : "avatar.png"}
+                          width="36" height="36"
+                          shape="rounded-circle" />
+                      </div>
+
+                      <div style={{ width: "80%", float: 'left' }}>
+                        <strong style={{ fontSize: 'small' }}> {item.author}</strong>
+                      </div>
+                    </div>
+
+                    <div style={{ width: "80%" }}>
+                      <p style={{ color: "#00000066", fontSize: 'smaller', marginBottom: '4px' }}>{<FormatTimestamp seconds={item.createdAt.seconds} />}</p>
+                    </div>
+
+                    <div style={{ width: "100%" }}>
+                      <hr></hr>
+                    </div>
+
+                    <div style={{ width: "100%", fontSize: 'small' }}>
+                      <p>{item.commentContent}</p>
+                    </div>
+                  </div>
+                  {/* </CCardBody>
+                  </CCard> */}
+                </td>
+              ),
+            'author':
+              (item) => (
+                <td
+                  style={{ display: "none" }}>
+                </td>
+              ),
+            'content':
+              (item) => (
+                <td
+                  style={{ display: "none" }}>
+                </td>
+              ),
+            'createdAt':
+              (item) => (
+                <td
+                  style={{ display: "none" }}>
+                </td>
+              ),
+          }
+          }
+        />
+      )} />
+    </CCol>
+  )
+}
+
--- src/Tables/ProposalsTable.js
+++ src/Tables/ProposalsTable.js
@@ -10,6 +10,7 @@ import Swal from 'sweetalert2';
 import { CImg } from '@coreui/react';
 import { Route } from 'react-router';
 import { FormatTimestamp } from 'src/reusable/reusables';
+import { CommentsTable } from './CommentsTable';
 
 const proposalFields = [
   { key: 'card', label: "", sorter: false, filter: false },
@@ -26,36 +27,44 @@ export class ProposalsTable extends React.Component {
       proposals: [],
       proposalID: "",
       comments: [],
-      loading: false,
+      showComments: false,
+      proposalsLoading: false,
+      commentsLoading: false,
     };
   }
 
   componentDidMount() {
-    this.setState({ loading: true })
 
     const getProposals = async (db) => {
+      this.setState({ proposalsLoading: true })
+
       const proposalCol = collection(db, 'proposals/')
       const proposalSnapshot = await getDocs(proposalCol);
       const proposalList = proposalSnapshot.docs.map(doc => doc.data());
       this.setState({ proposals: proposalList });
-      this.setState({ loading: false });
+      this.setState({ proposalsLoading: false });
       console.log(this.state.proposals);
     };
-
     getProposals(firebaseDB);
   }
 
   getComment(proposalID) {
+    this.setState({ commentsLoading: true })
+    this.state.showComments = true;
+    this.setState({ showComments: true });
+
+    console.log(proposalID);
     const fetchComments = async (db) => {
       const commentsCol = collection(db, 'proposals/'.concat(proposalID).concat('/comments'))
       const commentsSnapshot = await getDocs(commentsCol);
       const commentsList = commentsSnapshot.docs.map(doc => doc.data());
       this.setState({ comments: commentsList });
-      this.setState({ loading: false })
+      this.setState({ commentsLoading: false })
       console.log(this.state.comments)
     };
     fetchComments(firebaseDB);
   }
+
 
 
 
@@ -72,99 +81,80 @@ export class ProposalsTable extends React.Component {
             <CDataTable
               items={this.state.proposals}
               fields={proposalFields}
-              loading={this.state.loading}
+              loading={this.state.proposalsLoading}
               header={false}
               tableFilter={{ 'placeholder': 'Search...' }}
               itemsPerPage={20}
               pagination
               clickableRows
               sorterValue="createdAt"
-              onRowClick={(item, index, col, e) => {
-                if (this.getCookie("userEmail") === item.email) {
-                  // history.push("/profile")
-                } else {
-                  history.push({
-                    // pathname: "/users-profile",
-                    // state: item
-                  })
-                }
-              }
-              }
               scopedSlots={{
                 'card':
                   (item) => (
                     <td>
                       <CCard style={{ padding: "0", margin: "0" }}>
                         <CCardBody>
+                          <div>
+                            <div style={{ width: '100%' }}>
+                              <div style={{ width: "20%", float: 'left', textAlign: "center", marginLeft: '-6px', marginRight: '6px' }}>
+                                <CImg src={(item.picture) ? item.picture : "avatar.png"}
+                                  width="44" height="44"
+                                  shape="rounded-circle" />
+                              </div>
 
-                          <div style={{ width: '100%' }}>
-                            <div style={{ width: "20%", float: 'left', marginLeft: '-6px', marginRight: '6px' }}>
-                              <CImg src={(item.picture) ? item.picture : "avatar.png"}
-                                width="44" height="44"
-                                shape="rounded-circle" />
+                              <div style={{ width: "80%", float: 'left' }}>
+                                <strong style={{ fontSize: 'medium' }}> {item.author}</strong>
+                              </div>
                             </div>
 
-                            <div style={{ width: "80%", float: 'left' }}>
-                              <strong style={{ fontSize: 'medium' }}> {item.author}</strong>
+                            <div style={{ width: "80%" }}>
+                              <p style={{ color: "#00000066", fontSize: 'small', marginBottom: '4px' }}>{<FormatTimestamp seconds={item.createdAt.seconds} />}</p>
                             </div>
-                          </div>
 
-                          <div style={{ width: "80%" }}>
-                            <p style={{ color: "#00000066", fontSize: 'small', marginBottom: '4px' }}>{<FormatTimestamp seconds={item.createdAt.seconds} />}</p>
-                          </div>
+                            <div style={{ width: "100%" }}>
+                              <hr></hr>
+                            </div>
 
-                          <div style={{ width: "100%" }}>
-                            <hr></hr>
-                          </div>
+                            <div style={{ width: "100%" }}>
+                              <p>{item.content}</p>
+                            </div>
 
-                          <div style={{ width: "100%" }}>
-                            <p>{item.content}</p>
-                          </div>
+                            <div style={{ width: "100%", textAlign: "end" }}>
+                              <p onClick={() => { this.getComment(item.proposalID) }} style={{ color: "#00000066", fontSize: 'smaller', marginBottom: '4px' }}>13 comments</p>
+                            </div>
 
-                          <div style={{ width: "100%", textAlign: "end" }}>
-                            <a href="www.google.com"><p style={{ color: "#00000066", fontSize: 'smaller', marginBottom: '4px' }}>13 comments</p></a>
-                          </div>
+                            <div style={{ width: "100%" }}>
+                              <hr></hr>
+                            </div>
 
-                          <div style={{ width: "100%" }}>
-                            <hr></hr>
-                          </div>
+                            <div style={{ width: "100%", textAlign: 'center' }}>
+                              <CButton
+                                style={{ margin: '0 10px 10px' }}
+                                size="sm"
+                                color="dark"
+                                variant="ghost"
+                                onClick={() => {
 
-                          <div style={{ width: "50%", float: 'left', textAlign: 'center' }}>
-                            <CButton
-                              style={{ margin: '5px' }}
-                              size="sm"
-                              color="dark"
-                              variant="ghost"
-                              onClick={() => {
+                                }}
 
-                              }}
+                              >Share <CIcon size={"lg"} content={cilShare} /></CButton>
 
-                            >Share <CIcon size={"lg"} content={cilShare} /></CButton>
-                          </div>
+                              <CButton
+                                style={{ margin: '0 10px 10px' }}
+                                size="sm"
+                                color="dark"
+                                variant="ghost"
+                                onClick={() => { }}
+                              >Comment <CIcon size={"lg"} content={cilCommentBubble} /></CButton>
+                            </div>
 
-                          <div style={{ width: "50%", float: 'left', textAlign: 'center' }}>
-                            <CButton
-                              style={{ margin: '5px' }}
-                              size="sm"
-                              color="dark"
-                              variant="ghost"
-                              onClick={() => {
-
-                              }}
-
-                            >Comment <CIcon size={"lg"} content={cilCommentBubble} /></CButton>
                           </div>
 
 
-
-
-
-
-
-                          {/* <CCol style={{ textAlign: 'end', paddingRight: '0' }}>
-                          <CBadge color={getInterestsBadge(item.areaOfInterest)}>{item.areaOfInterest}</CBadge>
-                        </CCol> */}
-
+                          {/* Comments */}
+                          <div style={{ display: (this.state.showComments) ? 'block' : 'none' }}>
+                            <CommentsTable comments={this.state.comments} loading={this.state.commentsLoading} showComments={this.state.showComments} />
+                          </div>
 
                         </CCardBody>
                       </CCard>
